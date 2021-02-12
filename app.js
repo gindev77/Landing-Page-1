1e8aa4c954064753aa883eb97e60fe39fe9b67c2
Gilles Giannini <gindev77@gmail.com>
2021-02-12 18:12:10 +0100

    align-self card-pres1 & 2 modifiée

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const menu = document.querySelector('.list-nav');
-// const item_list = document.querySelector('.item-nav');
 const btnMenu = document.querySelector('.btn-toggle-container');
 
 
 btnMenu.addEventListener('click', function() {
     menu.classList.toggle('active-menu');
-    // item_list.classList.toggle('active-menu');
 })
 
